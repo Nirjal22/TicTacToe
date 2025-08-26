83decffeb972111d0f3a6e6e629fcac6a899e485
Nirjal22 <bistanirjal978@gmail.com>
2025-08-26 21:42:56 +0545

    Driver file

---
+++ src/myPackage/Main.java
@@ -0,0 +1,9 @@
+package myPackage;
+
+public class Main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new MainMenu().show();
+        });
+    }
+}
